0f7bb1d611cc094b21949b6e28e6cd5e82cd4f42
leontres321 <leonardo.sobarzo@sansano.usm.cl>
2018-07-12 14:37:32 -0400

    Prueba inicial

--- Main.py
+++ Main.py
@@ -1,6 +1,20 @@
-import Melee
+from Melee import *
 import Monstruo
-import Aux
 
+#TODO guardar el filo en el arma y solo revisar el diccionario al crear esta
 if __name__ == '__main__':
-    print("Hola")
+    #(self, base, elem, filo, critico, clase)
+    SnS = Arma_melee(196, 520, "blanco", 0, 1.4)
+    filo_base = {"rojo": 0.5, "naranjo": 0.75, "amarillo": 1, "verde": 1.125, "azul": 1.25, "blanco": 1.3, "morado": 1.5}
+    filo_elem = {"rojo": 0.25, "naranjo": 0.5, "amarillo": 0.75, "verde": 1, "azul": 1.0625, "blanco": 1.125, "morado": 1.2}
+
+    #(self, tipo, filo_base, zona, defensa, ira, var)
+    dano_base = SnS.dano_base(0.18, filo_base[SnS.filo], 0.8, 1, 1, 1)
+
+    #(self, filo_elem, Ezona, defensa, ira, var)
+    dano_elem = SnS.dano_elem(filo_elem[SnS.filo], 0.3, 1, 1, 1)
+
+
+    print("Prueba de Sns saltando vs Rathalos G:")
+    print("Daño base: ", dano_base, ", Daño elemental: ", dano_elem)
+    print("Total: ", dano_base + dano_elem)
--- Melee.py
+++ Melee.py
@@ -1,6 +1,15 @@
-
+#Hacer esto para cada arma y contener los combos
 class Arma_melee:
-    def __init__(self, base, elem, filo):
+    #ver si poner o no var aca, por la falta de var
+    def __init__(self, base, elem, filo, critico, clase):
         self.base = base
         self.elem = elem
         self.filo = filo
+        self.critico = critico
+        self.clase = clase
+
+    def dano_base(self, tipo, filo_base, zona, defensa, ira, var):
+        return int(self.base * tipo * filo_base * zona * defensa * ira * var / self.clase)
+
+    def dano_elem(self, filo_elem, Ezona, defensa, ira, var):
+        return int(self.elem * filo_elem * Ezona * defensa * ira * var / 10)
