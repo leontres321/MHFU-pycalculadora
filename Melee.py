dfc3a4a6cff6b86ed436bc9e409d75b176480afc
leontres321 <leonardo.sobarzo@sansano.usm.cl>
2018-07-12 17:01:05 -0400

    Clase abstracta, intento 1

--- Melee.py
+++ Melee.py
@@ -1,15 +1,22 @@
 #Hacer esto para cada arma y contener los combos
-class Arma_melee:
+from abc import ABCMeta, abstractmethod
+
+class Arma_melee(metaclass = ABCMeta):
     #ver si poner o no var aca, por la falta de var
-    def __init__(self, base, elem, filo, critico, clase):
+    def __init__(self, base, elem, elem_dano, filo_base, filo_elem, critico, clase, var):
         self.base = base
+        self.elem_dano = elem_dano
         self.elem = elem
-        self.filo = filo
+        self.filo_base = filo_base
+        self.filo_elem = filo_elem
         self.critico = critico
         self.clase = clase
+        self.var = var
 
-    def dano_base(self, tipo, filo_base, zona, defensa, ira, var):
-        return int(self.base * tipo * filo_base * zona * defensa * ira * var / self.clase)
+    @abstractmethod
+    #TODO SACAR TIPO CUANDO LOGRE HACER DECENTE LOS ATAQUES
+    def dano_base(self, zona, defensa, ira):
+        pass
 
-    def dano_elem(self, filo_elem, Ezona, defensa, ira, var):
-        return int(self.elem * filo_elem * Ezona * defensa * ira * var / 10)
+    def dano_elem(self, Ezona, defensa, ira):
+        return int(self.elem * self.filo_elem * Ezona * defensa * ira * self.var / 10)
