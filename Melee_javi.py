ed2b08fe6ac35e9ec1936fba764da301f01fc7fd
javiera navarrete <jlnavarrete@uc.cl>
2020-01-24 17:36:32 -0300

    armas complejas

--- Melee_javi.py
+++ Melee_javi.py
@@ -2,24 +2,13 @@ from abc import ABCMeta, abstractmethod
 from parámetros import *
 class Melee(metaclass = ABCMeta):
     def __init__(self, ATP, SHARP, ELEMENT, TYPE_ELEMENT, TYPE, CLASS, VAR, C_I  ):  ####### TYPE * VAR pordria guardarse como uno solo
-        self.ATP = float(ATP)
-        self.SHARP = SHARP
-        self.ELEMENT = float(ELEMENT)
+
         self.TYPE_ELEMENT = TYPE_ELEMENT
         self.TYPE = TYPE
-        self.CLASS = float(CLASS)
-        self.VAR = float(VAR)
+        self.VAR = VAR
         self.C_I = C_I
-
-    def dano_base(self):
-        pass
-
-    def dano_elemental(self):
-        pass
-
-    def dano_cortante(self):
-        pass
-
+        self.AUX_BASE = float(ATP) * base_sharp[SHARP] / float(CLASS)
+        self.AUX_ELEM = float(ELEMENT) * elemental_sharp[SHARP] / divisor
 
     def dano_total(self,monster):
         lista_danos = [["---"]]
@@ -32,14 +21,53 @@ class Melee(metaclass = ABCMeta):
                     lista_danos[0].append(zona)
 
                 if self.C_I  == "Both":
-                    dano_base = max((self.ATP * self.TYPE[mov]* base_sharp[self.SHARP] * zona[hit_dict["Cut"]] * self.VAR ) / self.CLASS , (self.ATP * self.TYPE[mov]* base_sharp[self.SHARP] * zona[hit_dict["Impact"]] * self.VAR ) / self.CLASS)
+                    dano_base = max(self.AUX_BASE * self.TYPE[mov] *  zona[hit_dict["Cut"]] * self.VAR ,
+                                    self.AUX_BASE * self.TYPE[mov] * zona[hit_dict["Impact"]] * self.VAR )
                 else:
-                    dano_base = (self.ATP * self.TYPE[mov] * base_sharp[self.SHARP] * zona[hit_dict[self.C_I]] * self.VAR) / self.CLASS
+                    dano_base = self.AUX_BASE * self.TYPE[mov] *  zona[hit_dict[self.C_I]] * self.VAR
 
                 dano_elemental = 0
 
                 if self.TYPE_ELEMENT != None:
-                    dano_elemental = (self.ELEMENT * elemental_sharp[self.SHARP] * zona[hit_dict[self.TYPE_ELEMENT]] * self.VAR ) / divisor
+                    dano_elemental = self.AUX_ELEM *  zona[hit_dict[self.TYPE_ELEMENT]] * self.VAR
+
+                fila_mov.append(dano_elemental + dano_base)
+            lista_danos.append(fila_mov)
+
+        return lista_danos
+
+    def dano_raro(self,monster):
+        lista_danos = [["---"]]
+
+        for mov in self.Type.keys():
+            fila_mov = [mov]
+
+            if "All" in self.VAR.keys():
+                var_1 = self.VAR["All"]
+            elif mov in self.VAR.keys():
+                var_1 = self.VAR[mov]
+                var_2 = self.VAR[mov]
+            elif mov in ["Charging Stab Finisher", "Charge Through"]:
+                var_1 = self.VAR["Charging Cut"]
+                var_2 = self.VAR["Charging Impact"]
+            else:
+                var_1 = 1
+                var_2= 1
+
+            for zona in monster.zonas:
+                if zona not in lista_danos[0]:
+                    lista_danos[0].append(zona)
+
+                if self.C_I == "Both":
+                    dano_base = max(self.AUX_BASE * self.TYPE[mov] * zona[hit_dict["Cut"]] * var_1,
+                                    self.AUX_BASE * self.TYPE[mov] * zona[hit_dict["Impact"]] * var_2)
+                else:
+                    dano_base = self.AUX_BASE * self.TYPE[mov] * zona[hit_dict[self.C_I]] * var_1
+
+                dano_elemental = 0
+
+                if self.TYPE_ELEMENT != None:
+                    dano_elemental = self.AUX_ELEM * zona[hit_dict[self.TYPE_ELEMENT]] * var_1 ## que var usa lanza segun si antes es cut o imp
 
                 fila_mov.append(dano_elemental + dano_base)
             lista_danos.append(fila_mov)
@@ -57,10 +85,21 @@ class ShieldNSword(Melee):
 class DualSword(Melee):
     def __init__(self,  ATP, SHARP, ELEMENT, TYPE_ELEMENT):
         super().__init__(ATP = ATP, SHARP = SHARP, ELEMENT = ELEMENT, TYPE_ELEMENT = TYPE_ELEMENT, TYPE = dual_sword_dict["Type"], CLASS =  dual_sword_dict["Class"], VAR =  dual_sword_dict["Var"], C_I= "Cut")
-
+"""
 class GreatSword(Melee):
     def __init__(self,  ATP, SHARP, ELEMENT, TYPE_ELEMENT):
         super().__init__(ATP = ATP, SHARP = SHARP, ELEMENT = ELEMENT, TYPE_ELEMENT = TYPE_ELEMENT, TYPE = great_sword_dict["Type"], CLASS = great_sword_dict["Class"], VAR = great_sword_dict["Var"], C_I= "Cut")
+"""
+class GunLance(Melee):
+    def __init__(self,  ATP, SHARP, ELEMENT, TYPE_ELEMENT):
+        super().__init__(ATP = ATP, SHARP = SHARP, ELEMENT = ELEMENT, TYPE_ELEMENT = TYPE_ELEMENT, TYPE = gunlance_dict["Type"], CLASS = gunlance_dict["Class"], VAR = gunlance_dict["Var"], C_I= "Cut")
 
+class Hammer(Melee):
+    def __init__(self, ATP, SHARP, ELEMENT, TYPE_ELEMENT):
+        super().__init__(ATP = ATP, SHARP = SHARP, ELEMENT = ELEMENT, TYPE_ELEMENT = TYPE_ELEMENT, TYPE = hammer_dict["Type"], CLASS = hammer_dict["Class"], VAR = hammer_dict["Var"], C_I= "Impact")
 
+class HuntingHorn(Melee):
+    def __init__(self, ATP, SHARP, ELEMENT, TYPE_ELEMENT):
+        super().__init__(ATP=ATP, SHARP=SHARP, ELEMENT=ELEMENT, TYPE_ELEMENT=TYPE_ELEMENT, TYPE=hunting_horn_dict["Type"],
+                         CLASS=hunting_horn_dict["Class"], VAR=hunting_horn_dict["Var"], C_I="Impact")
 
--- parámetros.py
+++ parámetros.py
@@ -2,42 +2,42 @@
 #shield and sword
 shield_n_sword_types = {"Regular Combo" : .46, "Revolving Slice" : .27, "Jumping Slice" : .18, "Rolling Slice" : .15, "Guard Attack 1" : .15, "Guard Attack 2" : .14, "Unsheathe Attack" : .18}
 
-shield_n_sword_dict = {"Var" : 1, "Type" : shield_n_sword_types, "Class" : 1.4}
+shield_n_sword_dict = {"Var" : {"All": 1} , "Type" : shield_n_sword_types, "Class" : 1.4}
 
 #Dual Swords (DS)
 dual_sword_types = {"Regular Combo" : .72, "Revolving Slice" : .31, "Charging Slice" : .23, "Rolling Slice" : .19, "Unsheathe attack" : .23 }
 
-dual_sword_dict = {"Var" : .70, "Type" : dual_sword_types, "Class" : 1.4}
+dual_sword_dict = {"Var" :{"All": .7}, "Type" : dual_sword_types, "Class" : 1.4}
 
 # Lance
 lance_types = {"Normal Stab Combo" : .76,"Upward Stab Combo" : .86, "Charge Through" : .20, "Charging Stab Finisher" : .40, "Guard Attack": .20,  "Unsheathe attack" : .28}
 
-lance_dict = {"Var" : [.72, 1.12, 0.81], "Type" : lance_types, "Class" : 2.3}
+lance_dict = {"Var" : {"Normal Stab Combo" :.72, "Upward Stab Combo" : .72, "Unsheathe attack" : .72, "Charging Cut" : 1.12, "Charging Impact" : 0.81}, "Type" : lance_types, "Class" : 2.3}
 
 # Gunlance (GL)
 gun_lance_types = {"Normal Stab Combo" : .82,"Upward Stab Combo" : .54, "Rushing Stab" : .34, "Upward Swing": .30,  "Unsheathe attack" : .34}
 
-gunlance_dict = {"Var" : .95, "Type" : gun_lance_types, "Class" : 2.3}
+gunlance_dict = {"Var" : {"All": .95}, "Type" : gun_lance_types, "Class" : 2.3}
 
 #Great Sword (GS)
 great_sword_types = {"Regular Slice" : .48, "Sideways Slash" : .36, "Upswing" : .46, "Level 1 Charge" : .65, "Level 2 Charge" : .80, "Level 3 Charge" : 1.10, "Unsheathe attack" : .48 }
 
-great_sword_dict = {"Var" : [1.05, 1.10, 1.20, 1.30], "Type" : great_sword_types, "Class" : 4.8}
+great_sword_dict = {"Var" : {"Center" : 1.05, "Level 1" : 1.1, "Level 2" : 1.2, "Level 3" : 1.3}, "Type" : great_sword_types, "Class" : 4.8}
 
 # Long Sword (LS)
 long_sword_types = {"Forward Slice" : .61, "Piercing Stab" : .20, "Sweeping Slice" : .30, "Upward Slice" : .23, "Downward Slice" : .28, "No Spirit Attack" : .16, "Spirit Attack" : 1.47, "Unsheathe Attack" : .33 }
 
-long_sword_dict = {"Var" : [1.05, 1.12], "Type" : long_sword_types, "Class" : 4.8}
+long_sword_dict = {"Var" : {"Center" : 1.05, "Spirit Gauge" : 1.12}, "Type" : long_sword_types, "Class" : 4.8}
 
 #  Hammer
 hammer_types = {"Regular Combo" : 1.72, "Side Swing" : .15, "Spinning Attack" : .40, "Short Charge" : .45, "Medium Charge" : .80, "Superpound" : .96, "Unsheathe attack" : .20}
 
-hammer_dict = {"Var" : 1, "Type" : long_sword_types, "Class" : 5.2}
+hammer_dict = {"Var" :{"All": 1}, "Type" : long_sword_types, "Class" : 5.2}
 
 #Hunting Horn (HH)
 hunting_horn_types = {"Swing Attack" : .27, "Poke Attack" : .36, "Overhead Smash" : .63, "Unsheathe attack" : .27 }
 
-hunting_horn_dict = {"Var" : 1, "Type" : long_sword_types, "Class" : 5.2}
+hunting_horn_dict = {"Var" : {"All": 1}, "Type" : long_sword_types, "Class" : 5.2}
 
 
 divisor = 10
